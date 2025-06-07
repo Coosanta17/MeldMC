80c99d118e2ae8b442dd81b0fbd2e2d017164602
Coosanta <coosantamapy@gmail.com>
2025-06-07 11:33:08 +1000

    Button Panel is now panelling buttons.

--- launcher/src/main/java/net/coosanta/meldmc/gui/serverselection/ButtonPanel.java
+++ launcher/src/main/java/net/coosanta/meldmc/gui/serverselection/ButtonPanel.java
@@ -7,8 +7,26 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import net.coosanta.meldmc.gui.button.MinecraftButton;
 
+import java.util.List;
+
 public class ButtonPanel extends GridPane {
     public ButtonPanel() {
+        configureGrid();
+        addButtons(List.of(
+                new MinecraftButton("Join Server", true),
+                new MinecraftButton("Server Info", true),
+                new MinecraftButton("Add Server")
+        ), 0, 4);
+
+        addButtons(List.of(
+                new MinecraftButton("Edit", true),
+                new MinecraftButton("Delete", true),
+                new MinecraftButton("Refresh"),
+                new MinecraftButton("Settings")
+        ), 1, 3);
+    }
+
+    private void configureGrid() {
         setHgap(10);
         setVgap(10);
         setPadding(new Insets(15));
@@ -20,21 +38,14 @@ public class ButtonPanel extends GridPane {
             column.setPercentWidth(100.0 / 12);
             getColumnConstraints().add(column);
         }
+    }
 
-        for (int i = 0; i < 3; i++) {
-            MinecraftButton button = new MinecraftButton("Button " + (i + 1));
+    private void addButtons(List<MinecraftButton> buttons, int row, int colspan) {
+        for (int i = 0; i < buttons.size(); i++) {
+            MinecraftButton button = buttons.get(i);
             button.setPrefHeight(50);
-            button.setMaxWidth(Double.MAX_VALUE);
             GridPane.setFillWidth(button, true);
-            add(button, i * 4, 0, 4, 1);
-        }
-
-        for (int i = 0; i < 4; i++) {
-            MinecraftButton button = new MinecraftButton("Button " + (i + 4));
-            button.setPrefHeight(50);
-            button.setMaxWidth(Double.MAX_VALUE);
-            GridPane.setFillWidth(button, true);
-            add(button, i * 3, 1, 3, 1);
+            add(button, i * colspan, row, colspan, 1);
         }
     }
 }
